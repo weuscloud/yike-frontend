27c6ec2c2b89d08ed14ae601c460c4d358afd24e
wangqicheng <weuscloud@gmail.com>
2023-05-06 16:13:14 +0800

    [develop] 1.1.7 bf

--- src/api/settings.js
+++ src/api/settings.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Set the base URL
-axios.defaults.baseURL = "http://localhost/api";
+axios.defaults.baseURL = "/api";
 
 axios.defaults.validateStatus = function (status) {
     return true; // 返回true表示不抛出错误
